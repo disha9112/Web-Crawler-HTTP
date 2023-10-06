a2008e067e443d4c73731d66c4271aa7c4dbfb45
disha9112 <disha9112@gmail.com>
2023-10-06 13:33:33 +0530

    getURLsFromHTML function and tests done

--- crawl.js
+++ crawl.js
@@ -1,3 +1,32 @@
+const { JSDOM } = require("jsdom");
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  // return all the clickable links of a webpage as an array of links
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === "/") {
+      // relative URL
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`Error with relative URL: ${err.message}`);
+      }
+    } else {
+      // absolute URL
+      try {
+        const urlObj = new URL(linkElement.href);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`Error with absolute URL: ${err.message}`);
+      }
+    }
+  }
+  return urls;
+}
+
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
   // trim the trailing slashes
@@ -10,4 +39,5 @@ function normalizeURL(urlString) {
 
 module.exports = {
   normalizeURL,
+  getURLsFromHTML,
 };
